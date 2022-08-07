ed82e7eff1efa08df5165dbdb558a86e37b53dd7
Abhinay Thakur <cabhinay786@gmail.com>
2022-08-07 08:50:51 -0700

    todo compleated

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -5,45 +5,42 @@ function Todo() {
 
     const [task, setTask] = useState("add some task");
     const [data, setData] = useState([]);
+    
     const [editId, setEditId] = useState(0);
 
-    const onChangeHandler = (e) => {
-        setTask(e.target.value)
-    }
+  
 
     const submitHandler = (e) => {
         e.preventDefault();
 
-        // if (editId) {
-        //     const editTask = data.find((curEle,index) => index.id === editId);
-        //     const updatedData = data.map((t) =>
-        //       t.id === editTask.id
-        //         ? (t = { id: t.id, task })
-        //         : { id: t.id, task: t.task }
-        //     );
-        //     setData(updatedData);
-        //     setEditId(0);
-        //     setTask("");
-        //     return;
-        //   }
+        if (editId) {
+            const editTask = data.find((i) => i.id === editId);
+            const updatedData = data.map((t) =>
+              t.id === editTask.id
+                ? (t = { id: t.id, task })
+                : { id: t.id, task: t.task }
+            );
+            setData(updatedData);
+            setEditId(0);
+            setTask("");
+            return;
+          }
 
-        const newData = task;
-        setData([...data, newData])
+        if (task !== "") {
+            setData([{ id: `${task}-${Date.now()}`, task }, ...data]);
+            setTask("");
+          }
+        };
 
-        setTask('')
+    const deleteItem =(id)=>{
+        const delTask = data.filter((to) => to.id !== id);
+        setData([...delTask]);
     }
 
-    const deleteItem =(a)=>{
-        const finalData = data.filter((curEle,index)=>{
-            return index !== a;
-        })
-        setData(finalData)
-    }
-
-    const handleEdit = (a) => {
-        const editTask = data.find((curEle,index) => index.id === a);
+    const handleEdit = (id) => {
+        const editTask = data.find((i) => i.id === id);
         setTask(editTask.task);
-        setEditId(a);
+        setEditId(id);
       };
     return (
         <div className="container">
@@ -57,17 +54,17 @@ function Todo() {
                     <form onSubmit={submitHandler} >
                         <div className="row justify-content-between text-white p-2">
                             <div className="form-group flex-fill mb-2 col-9">
-                                <input id="todo-input" type="text" className="form-control"  value={task} onChange={onChangeHandler} required />
+                                <input id="todo-input" type="text" className="form-control"  value={task} onChange={(e) => setTask(e.target.value)} required />
                             </div>
                             <button type="submit" className="btn btn-primary mb-2 ml-2 col-3">{editId ? "Edit" : "ADD"}</button>
                         </div>
                     </form>
 
-                    {data.map((value, index) => {
+                    {data.map((t) => {
                         return <ShowTodo
-                            key={index}
-                            id={index}
-                            task={value}
+                            key={t.id}
+                            id={t.id}
+                            task={t.task}
                             
                             onSelcet={deleteItem}
                             handleEdit={handleEdit}
